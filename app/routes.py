1ac0022ec58cfe6ced69f9ec302d7e2ac332f2e0
ayaka <ayakaob@gmail.com>
2021-10-15 11:15:09 -0700

    create class Planet and list of instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,20 @@
 from flask import Blueprint
 
+
+class Planet:
+    def __init__(self, id, name, description, has_moons=True):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.has_moons = has_moons
+
+planets = [
+    Planet(1, "Earth", "Has Humans"),
+    Planet(2, "Mars", "No Humans"),
+    Planet(3, "Neptune", "No Humans"),
+    Planet(4, "Saturn", "No Humans"),
+    Planet(5, "Venus", "No Humans"),
+    Planet(6, "Uranus", "No Humans"),
+    Planet(7, "Mercury", "No Humans"),
+    Planet(8, "Jupiter", "No Humans"),
+] 
